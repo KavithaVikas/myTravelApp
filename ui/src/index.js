afdfb0ad5189cf155fc986ba0ca1f3bab81be833
Kavitha <xx18@global.tesco.org>
2017-06-24 23:51:06 +0530

    Added welcome message

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+import React from 'react';
+import {render} from 'react-dom';
+
+export default class Login extends React.Component{
+    render(){
+        return <h1>Welcome to my travel app</h1>
+    }
+}
+
+render (<Login/>, document.getElementById('app'));
+
